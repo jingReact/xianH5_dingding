b2e0efe98678190af218cffc55a1f6b9a2cc0759
liyingjing <ll47991367@qq.com>
2023-05-18 14:46:41 +0800

    ;s212

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,10 @@
-/*
- * @Author: liyingjing ll47991367@qq.com
- * @Date: 2023-05-11 19:03:37
- * @LastEditors: liyingjing ll47991367@qq.com
- * @LastEditTime: 2023-05-17 19:31:10
- * @FilePath: \vue3-vite2-ts-blog-h5\src\router\index.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
+
+
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Tabbar from "../components/tabbar";
-// 先识别所有的views/文件夹name/*.vue文件
-// 这里限制性很高，只有路径为/views/文件夹name/*.vue，的文件才能背识别
 const modules = import.meta.glob("../views/*/*.vue");
 const loadComponent = (component: string) =>
   modules[`../views/${component}.vue`];
-
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/home",
@@ -91,7 +82,8 @@ const router = createRouter({
 
 router.beforeEach((to) => {
   if (to.meta && to.meta.title) {
-    document.title = to.meta.title as string;
+    console.log( to,999);
+    document.title = to.meta.title as string
   }
 });
 
--- src/utils/setTittle.ts
+++
@@ -1,8 +0,0 @@
-
-export default function getPageTitle(pageTitle:string) {
-    if (pageTitle) {
-      return `${pageTitle} - ${title}`;
-    }
-    return `${title}`;
-    
-  }
